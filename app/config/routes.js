6e1e613f1488a69a766e970d1eb4369e79d53638
MagentaReds <grantodaniels@gmail.com>
2017-06-01 17:38:02 -0500

    Set up react routes using v4

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,9 +1,13 @@
 var React = require("react");
-var router = require("react-router");
+var router = require("react-router-dom");
 
 var Route = router.Route;
-var Router = router.Router;
-var hashHistory = router.hashHistory;
+
+//new in v4 react-router-dom, replaces the need for <Router history={hashHistory}>
+//used for modern HTML5 browsers use HashRouter for legacy browsers
+var BrowserRouter = router.BrowserRouter;
+//var hashHistory = router.hashHistory;
+
 var IndexRoute = router.IndexRoute;
 
 var Main = require("../components/Main.js");
@@ -11,14 +15,12 @@ var Search = require("../components/children/Search.js");
 var Results = require("../components/children/Results.js");
 var Saved = require("../components/children/Saved.js");
 
-module.exports = {
 
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      <Route path="results" component={Results} />
-      <Route path="saved" component={Saved} />
-      <IndexRoute component={Results} />
-    </Route>
-  </Router>
+module.exports = (
 
-};
+  <BrowserRouter>
+      <Route path="/" component={Main}>
+      </Route>
+  </BrowserRouter>
+
+);
