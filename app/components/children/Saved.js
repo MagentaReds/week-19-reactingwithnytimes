0d9a65b663e6bb79340ada131543daaad3887f03
MagentaReds <grantodaniels@gmail.com>
2017-06-01 20:19:07 -0500

    About to start work on these

--- app/components/children/Saved.js
+++ app/components/children/Saved.js
@@ -7,8 +7,21 @@ import React, {Component} from "react";
 class Saved extends Component {
   render() {
     return (
-      <div>
-        <h1> Saved Component </h1>
+      <div className="panel panel-primary">
+        <div className="panel-heading panelBack">
+          <span className="glyphicon glyphicon-paperclip">Saved Articles</span> 
+        </div>
+        <div id="our-results" className="panel-body">
+          
+          <div id="result_1" className="well">
+            <h2><span className="btn btn-primary">1</span> The Art oF Fake</h2>
+            <p>by BARNET L. LICHENSTEIN</p>
+            <p>Section: Opinon</p>
+            <p>9817329857239587239587</p>
+            <a>http://www.nytimes.com/2017/02/01/opinion/the-art-of-the-deal-in-afghanistan.html</a>
+          </div>
+            
+        </div>
       </div>
     );
   }
