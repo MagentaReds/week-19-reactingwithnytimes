52facb50538217f0fb898265373d0bbc2a2219dd
MagentaReds <grantodaniels@gmail.com>
2017-06-06 06:32:04 -0500

    Changed imports to ES6 syntax

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,19 +1,10 @@
-var React = require("react");
-var router = require("react-router-dom");
-
-var Route = router.Route;
-
 //new in v4 react-router-dom, replaces the need for <Router history={hashHistory}>
 //used for modern HTML5 browsers use HashRouter for legacy browsers
-var BrowserRouter = router.BrowserRouter;
 //var hashHistory = router.hashHistory;
 
-var IndexRoute = router.IndexRoute;
-
-var Main = require("../components/Main.js");
-var Search = require("../components/children/Search.js");
-var Results = require("../components/children/Results.js");
-var Saved = require("../components/children/Saved.js");
+import Main from "../components/Main.js";
+import React from "react";
+import {Route, BrowserRouter} from "react-router-dom";
 
 
 module.exports = (
